7c1d451496ca191c181f01d3dd9a9445852954ec
OSHIN <158509127+JaoShiGitHub@users.noreply.github.com>
2024-09-01 11:56:49 +0700

    Fetch data

--- app/components/pokemons.js
+++ app/components/pokemons.js
@@ -1,5 +1,24 @@
+"use client";
+
+import { useState, useEffect } from "react";
+
 const POKEMON_URL = "https://pokeapi-proxy.freecodecamp.rocks/api/pokemon/";
 
 export default function Pokemons() {
-  return <div>Pokemon</div>;
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    async function fetchPokemons() {
+      const getPokemons = await fetch(
+        `https://pokeapi-proxy.freecodecamp.rocks/api/pokemon/1`
+      );
+      const pokemons = await getPokemons.json();
+      setData(pokemons);
+    }
+    fetchPokemons();
+  }, []);
+
+  console.log(data);
+
+  return <div></div>;
 }
