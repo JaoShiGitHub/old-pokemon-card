b5d1256c624bc1f67a70808b058fa0bacbf43fab
OSHIN <158509127+JaoShiGitHub@users.noreply.github.com>
2024-10-09 17:05:03 +0700

    Test Input

--- app/components/SearchInput.js
+++ app/components/SearchInput.js
@@ -1,11 +1,31 @@
+"use client";
+
+import { useState, useEffect } from "react";
+const POKEMON_URL = "https://pokeapi-proxy.freecodecamp.rocks/api/pokemon/";
+
 export default function SearchInput() {
+  const [searchPokemon, setSearchPokemon] = useState("");
+  const [pokemonData, pokemonData] = useState([]);
+
+  function handleSetSearchPokemon(e) {
+    setSearchPokemon(e.target.value);
+    console.log(searchPokemon);
+  }
+
+  function handleSearchBtn() {
+    console.log("keyword: ", searchPokemon);
+  }
+  
   return (
     <div className="flex w-full justify-between">
       <input
         className="w-full bg-card pr-7 py-2 text-white outline-none"
+        type="text"
         placeholder="Search for Pokémon Name or ID"
+        value={searchPokemon}
+        onChange={handleSetSearchPokemon}
       />
-      <button>🔍</button>
+      <button onClick={handleSearchBtn}>🔍</button>
     </div>
   );
 }
